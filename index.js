044875d78ada34137c57cfde1922103069a39a7e
Isaac Aftergut <ijagut@vt.edu>
2023-09-21 11:46:49 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const pg = require("pg")
+const client = new pg.Client('postgres://localhost/fullstack_template_db')
+const express = require("express")
+const app = express();
+
+const init = async()=>{
+    await client.connect()
+    console.log("connected to db")
+    const SQL=`
+        SQL SETUP AND SEED
+    `;
+
+    const port = process.env.PORT || 3300;
+    app.listen(port, ()=>{
+        console.log(`listening on port ${port}`)
+    })
+}
+init()
